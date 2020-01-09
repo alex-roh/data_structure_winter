63c138c3236a7c982aeffb21083ca38ff8acc10b
eunyoungroh <roheunyoung@outlook.com>
2020-01-10 01:26:58 +0900

    보드 게임 beta
    1. 비주얼 스튜디오상에서 printf()를 할 때마다 프로그램 비상 종료
     : 힙을 건들 일이 없는데 힙 커럽션이 뜨는 이유가 의문.
    2. terminate() 함수가 정상 동작하지 않음.
    
    목표: 비주얼 스튜디오상에서 무사히 컴파일하기.

---
+++ hide_and_seek_devcpp/header.h
@@ -0,0 +1,36 @@
+#pragma once
+
+#define NONE 0         // 아무것도 없는 노드
+#define BOMB -1         // 폭탄의 고유 번호
+#define PLAYER1 1       // 플레이어 1의 고유 번호
+#define PLAYER2 2       // 플레이어 2의 고유 번호
+#define TWOGETHER 3     // 플레이어 1과 플레이어 2가 같은 노드에 있을 때의 고유 번 
+
+#define SIZE 12         // 노드의 개수
+#define PLAYERS 2       // 플레이어의 수
+#define BOMBS 3         // 폭탄의 개수
+
+#define LIFE 3          // 라이프 개수
+
+#define MAX 20          // 문자열의 최대호 길이
+
+#define INTROMESSAGE 1  // 인트로 메세지
+#define STATUSMESSAGE 2 // 상태 메세지
+
+
+#define FORWARD 1       // 정방향 이동
+#define BACKWARD -1      // 역방향 이동
+
+typedef struct node* node_ptr;
+
+node_ptr initialize();
+void traverse();
+void printBoard();
+void game_logic();
+void setBombs(node_ptr list);
+void roll_the_dice(int* dice1, int* dice2);
+void terminate();
+void printMessage(int type);
+void move_player(int player, int movement);
+void debug();
+void check_failure(int player);
